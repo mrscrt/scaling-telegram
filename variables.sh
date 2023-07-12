086a6ae5f9fc8c035eddd1105099b96ced7ebf57
mrscrt <139079575+mrscrt@users.noreply.github.com>
2023-07-12 13:46:24 +0300

    Add files via upload

---
+++ variables.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+# Variables (do not start with numbers)
+first_name=Timerkhan
+second_name=Giniyatullin
+val=10
+echo The first name is $first_name
+echo The second name is $second_name
+echo value $val
+
+
